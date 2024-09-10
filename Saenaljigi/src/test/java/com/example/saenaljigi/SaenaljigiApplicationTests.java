826ec6a500c6e954c27d16d81cacdfa695b04eb1
lulyulalla <daye200@naver.com>
2024-09-10 17:38:08 +0900

    build(setting):기본 세팅

---
+++ Saenaljigi/src/test/java/com/example/saenaljigi/SaenaljigiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saenaljigi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaenaljigiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
