b6b3aa0739c16b40e9835082bd6af17e8485ecb9
daye200 <daye200@naver.com>
2024-09-24 17:42:45 +0900

    feat(초기설정):초기 구조 변화

---
+++ src/main/java/com/example/saenaljigi/SaenaljigiApplication.java
@@ -0,0 +1,13 @@
+package com.example.saenaljigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaenaljigiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaenaljigiApplication.class, args);
+    }
+
+}
